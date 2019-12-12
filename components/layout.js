8e098c4ddf91a02129190acb2ee24fd1d8e79114
Angel Tello <angelst@gmail.com>
2019-12-11 20:56:12 -0600

    Next Head implemented for title page

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,11 @@
+import Head from 'next/head';
 import NavBar from './navbar';
 
 const Layout = (props) => (
     <div>
+        <Head>
+            <title>Next JS Sampla App</title>
+        </Head>
         <NavBar />
         {props.children}
     </div>
